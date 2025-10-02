09129f2303c165df72f335e2954d013fb5c73a3b
Ian Gurgel <iangurgel25@hotmail.com>
2025-10-02 15:54:42 -0300

    mudanças no handleSubmit do AddProductCompFormModal

--- src/components/(ProductsPageComponents)/AddProductCompFormModal.tsx
+++ src/components/(ProductsPageComponents)/AddProductCompFormModal.tsx
@@ -12,31 +12,50 @@ export default function AddProductCompForm() {
 
 
     const inputFields = [
-        { label: 'Nome', inputType: 'text', id: 'name', name: 'name', value: name,
+        {
+            label: 'Nome', inputType: 'text', id: 'name', name: 'name', value: name,
             onChangeInputFunc: (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)
         },
-        { label: 'Categoria', inputType: 'text', id: 'category', name: 'category', value: category,
+        {
+            label: 'Categoria', inputType: 'text', id: 'category', name: 'category', value: category,
             onChangeInputFunc: (e: React.ChangeEvent<HTMLInputElement>) => setCategory(e.target.value)
         },
-        { label: 'Quantidade', inputType: 'number', id: 'quantity', name: 'quantity', value: quantity,
+        {
+            label: 'Quantidade', inputType: 'number', id: 'quantity', name: 'quantity', value: quantity,
             onChangeInputFunc: (e: React.ChangeEvent<HTMLInputElement>) => setQuantity(Number(e.target.value))
         },
-        { label: 'Preço', inputType: 'number', id: 'price', name: 'price', value: price,
+        {
+            label: 'Preço', inputType: 'number', id: 'price', name: 'price', value: price,
             onChangeInputFunc: (e: React.ChangeEvent<HTMLInputElement>) => setPrice(Number(e.target.value))
         },
     ];
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if(!name || !category || !quantity || !price){alert("Preencha todos os campos"); return}
-        axios.post("http://localhost:8080/api/produtos", {
-            name,
-            category,
-            quantity,
-            price
-        });
-        toast.success("Produto cadastrado com sucesso!")
+        if (!name || !category || !quantity || !price) { 
+            toast.error("Preencha todos os campos"); 
+            return;
+        }
+        try {
+            await axios.post("http://localhost:8080/api/produtos", {
+                name,
+                category,
+                quantity,
+                price
+            });
+            toast.success("Produto adicionado com sucesso");
+        } catch(err) {
+            console.error(err);
+            toast.error("Erro ao adicionar produto")
+        }
     };
+
+    const handleClearInputs = () => {
+        setCategory("");
+        setName("");
+        setPrice(0);
+        setQuantity(0);
+    }
 
     return (
         <form onClick={(e) => e.stopPropagation()} onSubmit={handleSubmit} className="space-y-4">
@@ -59,7 +78,7 @@ export default function AddProductCompForm() {
             )}
 
             <div className="flex gap-x-2 justify-end">
-                <button type="button" className="bg-white text-[#2F80ED] border border-[#2F80ED] px-6 py-3 rounded-lg shadow hover:bg-[#EBF4FF] cursor-pointer">Descartar</button>
+                <button type="button" onClickCapture={handleClearInputs} className="bg-white text-[#2F80ED] border border-[#2F80ED] px-6 py-3 rounded-lg shadow hover:bg-[#EBF4FF] cursor-pointer">Descartar</button>
                 <button
                     type="submit"
                     className="cursor-pointer bg-[#2F80ED] text-white px-4 py-2 rounded-lg shadow hover:bg-[#1C64D1] duration-200 ease-out"
