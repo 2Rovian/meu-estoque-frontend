c0770af44e9ba1b0a2583e6fa4c00bf6c5270dd5
Ian Gurgel <iangurgel25@hotmail.com>
2025-10-01 21:14:24 -0300

    add método post, inputs funcionando

--- src/components/(ProductsPageComponents)/AddProductCompForm.tsx
+++ src/components/(ProductsPageComponents)/AddProductCompForm.tsx
@@ -1,15 +1,41 @@
+import axios from "axios";
+import { useState } from "react";
+
+
 export default function AddProductCompForm() {
 
+    const [name, setName] = useState<string>("");
+    const [category, setCategory] = useState<string>("");
+    const [quantity, setQuantity] = useState<number>(1);
+    const [price, setPrice] = useState<number>(0);
+
+
     const inputFields = [
-        { label: 'Nome', inputType: 'text', id: 'name', name: 'name' },
-        { label: 'Categoria', inputType: 'text', id: 'category', name: 'category' },
-        { label: 'Quantidade', inputType: 'number', id: 'quantity', name: 'quantity' },
-        { label: 'Preço', inputType: 'number', id: 'price', name: 'price' },
+        { label: 'Nome', inputType: 'text', id: 'name', name: 'name', value: name,
+            onChangeInputFunc: (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)
+        },
+        { label: 'Categoria', inputType: 'text', id: 'category', name: 'category', value: category,
+            onChangeInputFunc: (e: React.ChangeEvent<HTMLInputElement>) => setCategory(e.target.value)
+        },
+        { label: 'Quantidade', inputType: 'number', id: 'quantity', name: 'quantity', value: quantity,
+            onChangeInputFunc: (e: React.ChangeEvent<HTMLInputElement>) => setQuantity(Number(e.target.value))
+        },
+        { label: 'Preço', inputType: 'number', id: 'price', name: 'price', value: price,
+            onChangeInputFunc: (e: React.ChangeEvent<HTMLInputElement>) => setPrice(Number(e.target.value))
+        },
     ];
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-    }
+        if(!name || !category || !quantity || !price){alert("Preencha todos os campos"); return}
+        axios.post("http://localhost:8080/api/produtos", {
+            name,
+            category,
+            quantity,
+            price
+        });
+        alert("Produto cadastrado com sucesso!")
+    };
 
     return (
         <form onClick={(e) => e.stopPropagation()} onSubmit={handleSubmit} className="space-y-4">
@@ -22,8 +48,11 @@ export default function AddProductCompForm() {
                         id={field.id}
                         name={field.name}
                         type={field.inputType}
-                        min={0}
-                        className="mt-1 block w-full rounded-md border-2 border-[#E5E7EB] bg-white px-3 py-2 shadow-sm focus:border-[#2F80ED] sm:text-sm focus:outline-none duration-200 ease-in-out"
+                        value={field.value}
+                        onChange={field.onChangeInputFunc}
+                        min={1}
+                        maxLength={40}
+                        className="mt-1 block w-full rounded-md border-2 border-[#c0c2c5] bg-white px-3 py-2 shadow-sm focus:border-[#2F80ED] sm:text-sm focus:outline-none duration-200 ease-in-out"
                     />
                 </div>
             )}
