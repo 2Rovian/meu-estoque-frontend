9f75540c6cc4aff36607d5352c7437e15cc5e8b0
Ian Gurgel <iangurgel25@hotmail.com>
2025-10-04 10:41:41 -0300

    feito button 'Adicionar Produto' responsivo

--- src/components/(ProductsPageComponents)/AddProductComp.tsx
+++ src/components/(ProductsPageComponents)/AddProductComp.tsx
@@ -1,33 +1,37 @@
 import { useState } from "react"
 import ModalComp from "../ModalComp";
 import AddProductCompForm from "./AddProductCompFormModal";
-
+import { MdInventory2 } from "react-icons/md";
 
 export default function AddProductComp() {
 
-    const[ isModalOpen, setIsModalOpen ] = useState<boolean>(false);
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
     const handleCloseModal = () => {
         setIsModalOpen(prevIsModalOpen => !prevIsModalOpen);
     }
 
     return (
-        <div className="flex justify-between items-center mb-6">
+        <div className="flex items-stretch gap-x-2 mb-6">
             <input
                 type="text"
                 placeholder="Buscar produto..."
-                className="px-4 py-2 border-2 border-[#c0c2c5] focus:border-[#2F80ED] rounded-lg w-1/3 bg-white text-[#111827] focus:outline-none duration-200 ease-in-out
+                className="px-4 py-2 border-2 border-[#c0c2c5] focus:border-[#2F80ED] rounded-lg bg-white text-[#111827] w-full focus:outline-none duration-200 ease-in-out 
                 "
             />
-            <button onClick={() => setIsModalOpen(true)} className="bg-[#2F80ED] text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-[#1C64D1] shadow duration-200 ease-in-out">
-                Adicionar Produto
+            <button
+                onClick={() => setIsModalOpen(true)}
+                className="bg-[#2F80ED] text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-[#1C64D1] shadow duration-200 ease-in-out flex items-center whitespace-nowrap"
+            >
+                <MdInventory2  className="sm:mr-2 shrink-0" />
+                <span className="hidden sm:inline">Adicionar Produto</span>
             </button>
 
-            {isModalOpen && 
-            <ModalComp handleCloseModal={handleCloseModal}>
-                <AddProductCompForm />
-            </ModalComp>}
-            
+            {isModalOpen &&
+                <ModalComp handleCloseModal={handleCloseModal}>
+                    <AddProductCompForm />
+                </ModalComp>}
+
         </div>
     )
 }
