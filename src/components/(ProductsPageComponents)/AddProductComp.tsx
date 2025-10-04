024aeb2eb1eba978632579f4b9e553ff5a2c5785
Ian Gurgel <iangurgel25@hotmail.com>
2025-10-04 11:16:58 -0300

    feito 'handleSwitchBtn'

--- src/components/(ProductsPageComponents)/AddProductComp.tsx
+++ src/components/(ProductsPageComponents)/AddProductComp.tsx
@@ -3,12 +3,19 @@ import ModalComp from "../ModalComp";
 import AddProductCompForm from "./AddProductCompFormModal";
 import { MdInventory2 } from "react-icons/md";
 
+type buttonProps = "Nome" | "Categoria";
+
 export default function AddProductComp() {
 
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const [switchBtn, setSwitchBtn] = useState<buttonProps>("Nome");
 
     const handleCloseModal = () => {
         setIsModalOpen(prevIsModalOpen => !prevIsModalOpen);
+    }
+
+    const handleSwitchBtn = () => {
+        switchBtn === "Nome" ? setSwitchBtn("Categoria") : setSwitchBtn("Nome");
     }
 
     return (
@@ -19,7 +26,7 @@ export default function AddProductComp() {
                     placeholder="Buscar produto..."
                     className="focus:outline-none w-full group px-4 py-2"
                 />
-                <button className="text-[#2F80ED] border-l-2 border-transparent items-center flex h-full px-4 group-focus-within:border-[#2F80ED] duration-200 ease-in-out">Nome</button>
+                <button onClick={handleSwitchBtn} className="text-[#2F80ED] cursor-pointer border-l-2 border-transparent items-center flex h-full px-4 hover:bg-[#2F80ED] hover:text-white group-focus-within:border-[#2F80ED] duration-75 ease-in-out rounded-r-lg hover:outline-2 hover:outline-[#2F80ED]">{switchBtn}</button>
             </div>
             <button
                 onClick={() => setIsModalOpen(true)}
