b7a87de06906497821f754ecb197a91d9d72f170
Ian Gurgel <iangurgel25@hotmail.com>
2025-10-04 11:01:34 -0300

    feito UI do button

--- src/components/(ProductsPageComponents)/AddProductComp.tsx
+++ src/components/(ProductsPageComponents)/AddProductComp.tsx
@@ -13,17 +13,19 @@ export default function AddProductComp() {
 
     return (
         <div className="flex items-stretch gap-x-2 mb-6">
-            <input
-                type="text"
-                placeholder="Buscar produto..."
-                className="px-4 py-2 border-2 border-[#c0c2c5] focus:border-[#2F80ED] rounded-lg bg-white text-[#111827] w-full focus:outline-none duration-200 ease-in-out 
-                "
-            />
+            <div className="flex items-center justify-between border-2 border-[#c0c2c5] group focus-within:border-[#2F80ED] rounded-lg bg-white text-[#111827] w-full focus:outline-none duration-200 ease-in-out">
+                <input
+                    type="text"
+                    placeholder="Buscar produto..."
+                    className="focus:outline-none w-full group px-4 py-2"
+                />
+                <button className="text-[#2F80ED] border-l-2 border-transparent items-center flex h-full px-4 group-focus-within:border-[#2F80ED] duration-200 ease-in-out">Nome</button>
+            </div>
             <button
                 onClick={() => setIsModalOpen(true)}
                 className="bg-[#2F80ED] text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-[#1C64D1] shadow duration-200 ease-in-out flex items-center whitespace-nowrap"
             >
-                <MdInventory2  className="sm:mr-2 shrink-0" />
+                <MdInventory2 className="sm:mr-2 shrink-0" />
                 <span className="hidden sm:inline">Adicionar Produto</span>
             </button>
 
